6023ca57362f51d334028198be8746aac2e08b40
Joaquinzer <57538582+Joaquinzer@users.noreply.github.com>
2020-05-28 18:05:40 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -7,9 +7,46 @@
 using namespace std;
 
 int main(){
-  // Ejemplo prueba
-  Notas n('C',3);
-  saw s(true, 'L', 16000, 100);
-  s.ImprimirFreqs(n);
+  char nota;
+  int oct;
+  cout << "Ingresa la nota(En mayusculas): ";
+  cin >> nota;
+  cout << "Ingrese la octava: ";
+  cin >> oct;
+  Notas n(nota,oct);
+  bool f;
+  char tf;
+  float co;
+  string w;
+  cout << "agregar filtro? (1 si, 0 no): ";
+  cin >> f;
+  if(f){
+    cout << "tipo de filtro: (L para lowpass, H para highpass): ";
+    cin >> tf;
+    cout << "Ingresa el cutoff del filtro: ";
+    cin >> co;
+  }
+  else{
+    tf = 'N';
+    co = 22000;
+  };
+  Osc *o;
+  cout << "tipo de onda? (sine, square, saw): ";
+  cin >> w;
+  if(w == "sine"){
+    o = new Osc(f, tf, co, w, 100);
+    o->ImprimirFreqs(n);
+  }
+  else if(w == "square"){
+    o = new square(f, tf, co, 100);
+    o->ImprimirFreqs(n);
+  }
+  else if(w == "saw"){
+    o = new saw(f, tf, co, 100);
+    o->ImprimirFreqs(n);
+  }
+  else{
+    cout << "error" << endl;
+  }
   return 0;
 }
