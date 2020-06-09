87be9817fe0c8be771518689aa8ece65c9134dae
Joaquinzer <57538582+Joaquinzer@users.noreply.github.com>
2020-06-09 11:16:02 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -7,24 +7,22 @@
 using namespace std;
 
 int main(){
-  char nota;
-  int oct;
-  cout << "Ingresa la nota(En mayusculas): ";
-  cin >> nota;
-  cout << "Ingrese la octava: ";
-  cin >> oct;
-  Notas n(nota,oct);
   bool f;
   char tf;
   float co;
   string w;
+  cout <<  "Simula la frecuencias de un oscilador" << endl;
+  cout << "" << endl;
   cout << "agregar filtro? (1 si, 0 no): ";
   cin >> f;
+  cout << "" << endl;
   if(f){
     cout << "tipo de filtro: (L para lowpass, H para highpass): ";
     cin >> tf;
+    cout << "" << endl;
     cout << "Ingresa el cutoff del filtro: ";
     cin >> co;
+    cout << "" << endl;
   }
   else{
     tf = 'N';
@@ -33,20 +31,43 @@ int main(){
   Osc *o;
   cout << "tipo de onda? (sine, square, saw): ";
   cin >> w;
+  cout << "" << endl;
   if(w == "sine"){
     o = new sine(f, tf, co, 100);
-    o->ImprimirFreqs(n);
   }
   else if(w == "square"){
     o = new square(f, tf, co, 100);
-    o->ImprimirFreqs(n);
   }
   else if(w == "saw"){
     o = new saw(f, tf, co, 100);
-    o->ImprimirFreqs(n);
   }
   else{
     cout << "error" << endl;
+  }
+  char nota;
+  int oct;
+  Notas *n[100];
+  while (true){
+    int i = 1;
+    cout << "Ingresa la nota(En mayusculas): ";
+    cin >> nota;
+    cout << "" << endl;
+    cout << "Ingrese la octava: ";
+    cin >> oct;
+    cout << "" << endl;
+    n[i] = new Notas(nota,oct);
+    o->ImprimirFreqs(*n[i]);
+    i = i+1;
+    string sn;
+    cout << "Desea ingresar otra nota?(si/no) ";
+    cin >> sn;
+    cout << "" << endl;
+    if (sn == "no"){
+      break;
+    }
+    if (i == 101){
+      break;
+    }
   }
   return 0;
 }
